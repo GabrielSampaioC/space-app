fa2e1fbb591f32860d308e2818602dd9e140e79f
Gabriel Sampaio <108162858+GabrielSampaioC@users.noreply.github.com>
2024-01-14 22:14:05 -0300

    Importando o projeto Space-app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+import EstilosGlobais from "./componentes/EstilosGlobais"
+import Cabecalho from "./componentes/Cabecalho"
+import BarraLateral from "./BarraLateral"
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+
+export default function App(){
+  return (
+    <>
+    <FundoGradiente>
+      <EstilosGlobais/>
+      <Cabecalho/>
+      <BarraLateral/>
+    </FundoGradiente>
+
+
+    </>
+  )
+}
