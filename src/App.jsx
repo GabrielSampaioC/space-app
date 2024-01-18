e446903c036a750c3c7205369c37243ee4264cc2
Gabriel Sampaio <108162858+GabrielSampaioC@users.noreply.github.com>
2024-01-17 22:47:22 -0300

    Estruturando a seção de imagens

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import BarraLateral from "./componentes/BarraLateral"
 import Banner from "./componentes/Banner"
 import bannerBackground from "../public/assets/banner.png"
 import Galeria from "./componentes/Galeria"
+import fotos from "./fotos.json"
+import { useState } from "react"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -28,20 +30,23 @@ const ConteudoGaleria = styled.section`
   flex-direction: column;
   flex-grow: 1;
 `
-function App() {
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
+
   return (
+    
     <FundoGradiente>
       <EstilosGlobais />
       <AppContainer>
         <Cabecalho />
         <MainContainer>
           <BarraLateral />
-          <ConteudoGaleria>
+          <ConteudoGaleria >
             <Banner
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
-            <Galeria />
+            <Galeria fotos={fotosDaGaleria} />
           </ConteudoGaleria>
         </MainContainer>
 
--- src/componentes/Galeria/Populares/index.jsx
+++ src/componentes/Galeria/Populares/index.jsx
@@ -1,9 +1,9 @@
 import Titulo from "../../Titulo"
 
-const Populares = ()=> {
-    return(
+const Populares = () => {
+    return <div style={{ minWidth: 212 }}>
         <Titulo $alinhamento="center">Populares</Titulo>
-    )
+    </div>
 }
 
 export default Populares;
